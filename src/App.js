6ea548b70abba53f1ba589f2a1ea1272b8543c7e
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-03-27 12:19:22 -0300

    All functionality Done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [category, setCategory] = useState(filterData[0].title);
   async function fetchData() {
     setLoading(true);
     try{
@@ -33,18 +33,21 @@ const App = () => {
   
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-bgDark2">
       <div>
         <Navbar/>
       </div>
       <div className="bg-bgDark2">
         <div>
-          <Filter filterData = {filterData}/>
+          <Filter filterData = {filterData}
+          category={category}
+          setCategory={setCategory}
+          />
         </div>
         <div className="w-11/12 max-w-[1200px] mx-auto flex-wrap 
         flex justify-center items-center min-h-[50vh]">
           {
-            loading ?   (<Spinner/>) : (<Cards courses = {courses}/>)
+            loading ?   (<Spinner/>) : (<Cards courses = {courses} category={category}/>)
           }
         </div> 
       </div>
